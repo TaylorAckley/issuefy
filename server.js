f3ef643943e7105ab9d2d7c3c3e68e90d6c614a9
TaylorAckley <taylorackley@gmail.com>
2015-12-11 16:38:54 -0800

    config

--- server.js
+++ server.js
@@ -13,19 +13,16 @@ var moment          = require('moment');
 var request         = require('request');
 var qs              = require('querystring');
 var jwt             = require('jsonwebtoken'); // used to create, sign, and verify tokens
-if (!process.env.APP_URL) {
-var config          = require('./config'); // get our config file
-}
 var helpers         = require('./backend/helpers.js'); // get our helpers file
 var app             = express();
 
 //env vars
 var port            				= process.env.PORT || 3000;
-var APP_URL           			= process.env.APP_URL || config.APP_URL;
-var MONGOLAB_URI           	= process.env.MONGOLAB_URI || config.MONGOLAB_URI;
-var MAILGUN_API           	= process.env.MAILGUN_API || config.MAILGUN_API;
-var MAILGUN_DOMAIN          = process.env.MAILGUN_DOMAIN || config.MAILGUN_DOMAIN;
-var TOKEN_SECRET            = process.env.TOKEN_SECRET || config.TOKEN_SECRET;
+var APP_URL           			= process.env.APP_URL;
+var MONGOLAB_URI           	= process.env.MONGOLAB_UR;
+var MAILGUN_API           	= process.env.MAILGUN_API;
+var MAILGUN_DOMAIN          = process.env.MAILGUN_DOMAIN;
+var TOKEN_SECRET            = process.env.TOKEN_SECRET;
 
 //boring logging and parsing
 app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
