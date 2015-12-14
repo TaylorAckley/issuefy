40152a6af8e31db3951be0ad7187a448a609607a
TaylorAckley <taylorackley@gmail.com>
2015-12-14 11:48:26 -0800

    refactor

---
+++ public/app/config.js
@@ -0,0 +1,18 @@
+;(function() {
+
+
+	/**
+	 * Place to store API URL or any other constants
+	 * Usage:
+	 *
+	 * Inject CONSTANTS service as a dependency and then use like this:
+	 * CONSTANTS.API_URL
+	 */
+  angular
+  	.module('issuefy')
+    .constant('CONSTANTS', {
+      'API_URL': 'localhost:3000'
+    });
+
+
+})();
